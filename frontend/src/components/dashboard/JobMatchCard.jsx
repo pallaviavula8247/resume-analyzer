cec1852143de0698903706f3b355a6cf211e452e
pallavi <avulapallavi@gmail.com>
2026-07-31 18:07:07 +0530

    Added JobMatchCard component to dashboard

---
+++ frontend/src/components/dashboard/JobMatchCard.jsx
@@ -0,0 +1,27 @@
+function JobMatchPreview(){
+
+return(
+
+<div className="dashboard-box">
+
+<h2>Top Job Matches</h2>
+
+<ul>
+
+<li>Python Developer - 92%</li>
+
+<li>AI Engineer - 89%</li>
+
+<li>ML Engineer - 86%</li>
+
+<li>Backend Developer - 83%</li>
+
+</ul>
+
+</div>
+
+);
+
+}
+
+export default JobMatchPreview;
